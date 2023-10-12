7eba73dc5e7d00352b4c7fd8f5bce733d014038c
cos12378 <yrias8700@gmail.com>
2023-10-12 12:32:20 +0900

    first

---
+++ src/main/java/com/example/decoratordesign/DecoratorDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.decoratordesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoratorDesignApplication.class, args);
+    }
+
+}
